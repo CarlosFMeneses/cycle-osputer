d6a47503d7265840b5f81b1524c9275e315c9475
Carlos F. Meneses <11452413+CarlosFMeneses@users.noreply.github.com>
2026-05-24 06:37:48 -0400

    Initial Commit

---
+++ Cycle OSPuter/Cycle_OSPuterApp.swift
@@ -0,0 +1,17 @@
+//
+//  Cycle_OSPuterApp.swift
+//  Cycle OSPuter
+//
+//  Created by Carlos Meneses on 5/24/26.
+//
+
+import SwiftUI
+
+@main
+struct Cycle_OSPuterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
